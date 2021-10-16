a62f8feb4bf120ea5d14c722824f6b6ce746a117
sergiorodas <sergiorodasbj@hotmail.com>
2021-10-15 21:28:21 -0300

    Adds persistence of received images in MySQL database

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,10 +1,40 @@
-const { application } = require('express')
 const express = require('express')
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs')
 
 const router = express.Router()
 
+const diskStorage = multer.diskStorage({
+    destination: path.join(__dirname, '../images'),
+    filename: (req, file, cb) => {
+       cb(null, Date.now() + '-' + file.originalname) 
+    }
+})
+
+const fileUpload = multer({
+    storage: diskStorage
+})
+
 router.get('/', (req, res) => {
     res.send('Welcome to my image app')
+})
+
+router.post('/images/post', fileUpload.single('image'), (req, res) => {
+
+    req.getConnection((err, conn) => {
+        if (err) return res.status(500).send('server error')
+
+        const type = req.file.mimetype
+        const name = req.file.originalname
+        const data = fs.readFileSync(path.join(__dirname, '../images/' + req.file.filename))
+
+        conn.query('INSERT INTO image set ?', [{type, name, data}], (err, rows) => {
+            if (err) return res.status(500).send('server error')
+
+            res.send('image saved!')
+        })
+    })
 })
 
 module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 const express = require('express')
-
+const mysql = require('mysql')
+const myConnectionDB = require('express-myconnection')
 const routes = require('./routes/routes')
+const cors = require('cors')
 
 const app = express()
+
+app.use(myConnectionDB(mysql, {
+    host: 'localhost',
+    port:3306,
+    user: 'root',
+    password:'123456789',
+    database: 'images'
+}))
+
+app.use(cors())
 
 app.use(routes)
 
